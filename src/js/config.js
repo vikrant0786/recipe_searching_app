a3a5203af11e3d5c25c3305588ae7eb2d44c529b
vikrant0786 <vikranty2j@gmail.com>
2021-09-13 11:02:18 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fa3edc68-7c81-4a0e-a941-519e21a6217b';
+export const MODAL_CLOSE_SEC = 2.5;
